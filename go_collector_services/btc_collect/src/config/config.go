7a5b0cca1061fea87a1633ad0213fefd01ab4da7
realm520 <realm520@gmail.com>
2018-11-06 14:35:23 +0800

    support LTC in config

--- go_collector_services/btc_collect/src/config/config.go
+++ go_collector_services/btc_collect/src/config/config.go
@@ -16,12 +16,12 @@ type ServerConfig struct{
 	SafeBlock map[string]int
 	MULTISIGVERSION map[string]int
 }
-var RpcServerConfig = ServerConfig{RpcListenEndPoint:map[string]string{"BTC":"0.0.0.0:5444","BTC_TEST":"0.0.0.0:5446"},
-	SourceDataHost:map[string]string{"BTC":"http://btc_wallet","BTC_TEST":"http://192.168.1.124"},
-	SourceDataPort:map[string]string{"BTC":"60011","BTC_TEST":"10001"},
-	SourceDataUserName:map[string]string{"BTC":"a","BTC_TEST":"test"},
-	SourceDataPassword:map[string]string{"BTC":"b","BTC_TEST":"test"},
-	DbPathConfig:map[string]string{"BTC":"/hx/btc_collect_data","BTC_TEST":"h:/btc_collect_test/"},
-	SupportCoinType:map[string]string{"BTC":"","BTC_TEST":""},
-	SafeBlock:map[string]int{"BTC":6,"BTC_TEST":1},
-	MULTISIGVERSION:map[string]int{"BTC":5,"BTC_TEST":196}}
+var RpcServerConfig = ServerConfig{RpcListenEndPoint:map[string]string{"BTC":"0.0.0.0:5444","BTC_TEST":"0.0.0.0:5446", "LTC":"0.0.0.0:5445"},
+	SourceDataHost:map[string]string{"BTC":"http://btc_wallet","BTC_TEST":"http://192.168.1.124", "LTC":"http://ltc_wallet"},
+	SourceDataPort:map[string]string{"BTC":"60011","BTC_TEST":"10001", "LTC":"60012"},
+	SourceDataUserName:map[string]string{"BTC":"a","BTC_TEST":"test", "LTC":"a"},
+	SourceDataPassword:map[string]string{"BTC":"b","BTC_TEST":"test", "LTC":"b"},
+	DbPathConfig:map[string]string{"BTC":"/hx/btc_collect_data/","BTC_TEST":"h:/btc_collect_test/", "LTC":"/hx/ltc_collect_data/"},
+	SupportCoinType:map[string]string{"BTC":"","BTC_TEST":"", "LTC":""},
+	SafeBlock:map[string]int{"BTC":6,"BTC_TEST":1, "LTC":6},
+	MULTISIGVERSION:map[string]int{"BTC":5,"BTC_TEST":196,"LTC":5}}
