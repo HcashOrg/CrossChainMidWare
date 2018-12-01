970b69ca18599c25477cc6a7f8f69802a9b87c7f
sunnypickle <443237831@qq.com>
2018-12-01 15:08:55 +0800

    change erc conf

--- config/erc_conf.py
+++ config/erc_conf.py
@@ -1,12 +1,20 @@
 
 
 ercda_info = {
-    "precison" : "14",
-    "chainId" : "ERCDA",
-    "address" : "0xb99f8b924c272b51389c6322d05139eaed836938"
+    "precison": "14",
+    "chainId": "ERCDA",
+    "address": "0xb99f8b924c272b51389c6322d05139eaed836938"
 }
+ercpax_info = {
+    "precison" : "18",
+    "chainId" : "ERCPAX",
+    "address" : "0x8e870d67f660d95d5be530380d0ec0bd388289e1"
+}
+
 
 erc_map = {}
 erc_chainId_map = {}
 erc_map[ercda_info['address']] = ercda_info
 erc_chainId_map[ercda_info['chainId']] = ercda_info
+erc_map[ercpax_info['address']] = ercpax_info
+erc_chainId_map[ercpax_info['chainId']] = ercpax_info
