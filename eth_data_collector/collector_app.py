2cd9e8a50c83c4645fe2a045ca0160736e6d3cc0
sunnypickle <443237831@qq.com>
2018-12-03 14:02:39 +0800

    final fix log index bugs

--- eth_data_collector/collector_app.py
+++ eth_data_collector/collector_app.py
@@ -526,11 +526,11 @@ def trx_store_erc(db_pool,erc_trxs):
                     "index":erc_trx["logIndex"],
                     "fee": "0"
                 }
+                erc_in["index"] = hex(int(erc_in["index"]))[2:]
                 eth_in_data = yield db_pool.b_deposit_transaction.find_one(
                     {"txid": erc_in["txid"],
                      "index": erc_in["index"],
                      "asset_symbol": erc_in["asset_symbol"]})
-                erc_in["index"] = hex(int(erc_in["index"]))[2:]
                 if eth_in_data == None:
                     yield db_pool.b_deposit_transaction.insert(erc_in)
                 else:
@@ -584,9 +584,9 @@ def trx_store_erc(db_pool,erc_trxs):
                                                     erc_out['amount'] = str(TurnAmountFromEth(str(int(erc_trx["value"], 16)),int(erc_contract["precison"])))
                                                     erc_out['call_contract'] = json.loads(source_trx).get("result")["to"]
                                                         #str("%."+asset_account["precison"]+"lf"%(float(int(erc_trx["value"], 16)) / pow(10, int(erc_contract["precison"])))).rstrip('0')
+                                                    erc_out["index"] = hex(int(erc_out["index"]))[2:]
                                                     eth_out_data = yield db_pool.b_withdraw_transaction.find_one(
                                                         {"txid": erc_out["txid"], "index": erc_out["index"], "asset_symbol": erc_out["asset_symbol"]})
-                                                    erc_out["index"] = hex(int(erc_out["index"]))[2:]
                                                     if eth_out_data == None:
                                                         yield db_pool.b_withdraw_transaction.insert(erc_out)
                                                     else:
