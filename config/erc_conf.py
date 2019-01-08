03c3cd70fe7e88464b061662682eec88f46cedc9
Null-nil <530623363@qq.com>
2019-01-08 15:34:29 +0800

    ADD ERCELF

--- config/erc_conf.py
+++ config/erc_conf.py
@@ -11,6 +11,11 @@ ercpax_info = {
     "address" : "0x8e870d67f660d95d5be530380d0ec0bd388289e1"
 }
 
+ercelf_info = {
+    "precison" : "18",
+    "chainId" : "ERCELF",
+    "address" : "0xbf2179859fc6D5BEE9Bf9158632Dc51678a4100e"
+}
 
 erc_map = {}
 erc_chainId_map = {}
@@ -18,3 +23,5 @@ erc_map[ercda_info['address']] = ercda_info
 erc_chainId_map[ercda_info['chainId']] = ercda_info
 erc_map[ercpax_info['address']] = ercpax_info
 erc_chainId_map[ercpax_info['chainId']] = ercpax_info
+erc_map[ercelf_info['address']] = ercelf_info
+erc_chainId_map[ercelf_info['chainId']] = ercelf_info
