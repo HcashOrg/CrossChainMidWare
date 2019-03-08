02c8f548ca3ee92b53358e61119dd0c2430b256b
mutalisk <0x08@0x08.net>
2019-03-08 16:57:10 +0800

    support bytom

--- go_collector_services/btc_collect/src/util/link.go
+++ go_collector_services/btc_collect/src/util/link.go
@@ -88,7 +88,6 @@ func (client *LinkClient)SafeLinkHttpFunc(function string,params *[]interface{},
 			}
 		}
 		{
-
 			if index>=12{
 				fmt.Println("http request is error,please wait to retry,current sleep time is ",time.Second*time.Duration(sleepInterval[11]),time.Now())
 				time.Sleep(time.Second*time.Duration(sleepInterval[11]))
@@ -101,7 +100,29 @@ func (client *LinkClient)SafeLinkHttpFunc(function string,params *[]interface{},
 	}
 }
 
-
+func (client *LinkClient)SafeLinkHttpFuncForBTM(function string,params *map[string]interface{})(*simplejson.Json) {
+	sleepInterval :=[]int{5,10,20,30,40,60,120,240,480,960,1920,3840}
+	index :=0
+	for;;{
+		return_value:=client.LinkHttpFuncForBTM(function,params)
+		if return_value != nil{
+			_,exist:=return_value.CheckGet("result")
+			if exist{
+				return return_value
+			}
+		}
+		{
+			if index>=12{
+				fmt.Println("http request is error,please wait to retry,current sleep time is ",time.Second*time.Duration(sleepInterval[11]),time.Now())
+				time.Sleep(time.Second*time.Duration(sleepInterval[11]))
+			}else{
+				fmt.Println("http request is error,please wait to retry,current sleep time is ",time.Second*time.Duration(sleepInterval[index]),time.Now())
+				time.Sleep(time.Second*time.Duration(sleepInterval[index]))
+			}
+		}
+		index++
+	}
+}
 
 func (client * LinkClient)LinkHttpFunc(function string,params *[]interface{},istls bool)(*simplejson.Json) {
 	strParams := turnInterface(params)
@@ -194,4 +215,65 @@ func (client * LinkClient)HttpRpcFunction(function string,param *[]interface{})s
 	json.Unmarshal([]byte(string(body)),&tempparam)
 	fmt.Println(string(body))
 	return string(body)
+}
+
+func (client * LinkClient)LinkHttpFuncForBTM(function string, params *map[string]interface{})(*simplejson.Json) {
+	transport := http.Transport{
+		DisableKeepAlives: true,
+	}
+	clienta := http.Client{Transport: &transport,}
+
+	b,_:= json.Marshal(params)
+
+	payload := strings.NewReader(string(b))
+	//fmt.Println(payload)
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s:%s/%s", (*client).IP, (*client).Port, function), payload)
+
+	if err != nil{
+		fmt.Println("x")
+		fmt.Println(err.Error())
+		return nil
+	}
+	req.Header.Add("content-type", "text/plain")
+	res, err := clienta.Do(req)
+	if err != nil{
+		fmt.Println("1")
+		fmt.Println(err.Error())
+		return nil
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	if res.StatusCode!=200 {
+		fmt.Println("error status code " + strconv.Itoa(res.StatusCode))
+		return nil
+	}
+	if err != nil{
+		fmt.Println(err.Error())
+		return nil
+	}
+	if strings.Contains(string(body),"504 Gateway Time-out"){
+		fmt.Println(string(body))
+		return nil
+	}
+
+	tempParam := make(map[string]interface{})
+	json.Unmarshal([]byte(string(body)),&tempParam)
+	retParam := make(map[string]interface{})
+
+	if tempParam["status"].(string) == "success" {
+		retParam["result"] = tempParam["data"]
+		bs, _ := json.Marshal(retParam)
+		js, _ := simplejson.NewJson(bs)
+		return js
+	} else if tempParam["status"].(string) == "fail" {
+		retParam["error"] = tempParam["msg"]
+		bs, _ := json.Marshal(retParam)
+		js, _ := simplejson.NewJson(bs)
+		return js
+	} else {
+		return nil
+	}
+	return nil
 }
